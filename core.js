013a20f9cc5ba0f60b2f652f967ff4fc9c430ff0
GedFlow <ptj1327@naver.com>
2024-02-12 17:23:30 +0900

    [Feat]- core.js 내부 exec 함수를 promisify로 프로미스화하여 await을 사용. 절차지향적으로 만들어줌. Core를 호출한 코드는 Core 내부 로직이 모두 실행 완료되기 전까지 기다리게 됨.

--- core.js
+++ core.js
@@ -1,34 +1,28 @@
-// const { exec } = require('child_process');
+import { promisify } from 'util';
 import { exec } from 'child_process';
 
-const Core = async () => {
+const execAsync = promisify(exec);
+
+const Core = async ( musicTitle ) => {
+
   // 악보 이름
-  const nodeVariable_musicTitle = "light.jpg"; // 악보이름
-  process.env.NODE_VARIABLE_MUSIC_TITLE = nodeVariable_musicTitle; // 악보이름을 시스템 환경변수로 설정
+  process.env.NODE_VARIABLE_MUSIC_TITLE = musicTitle; // 악보이름을 시스템 환경변수로 설정
   
-  // PowerShell 스크립트 실행
-  exec(`powershell -Command ".\\script.ps1"`, (error, stdout) => {
-    if (error) {
-      console.error(`Error executing PowerShell script: ${error}`);
-      return;
-    }
-  
-    // PowerShell 스크립트의 결과 출력
-    console.log(`PowerShell script output:\n${stdout}`);
-  
-    // Python 가상환경 활성화 및 스크립트 실행
-    const pythonCommand = `.\\newenv\\Scripts\\activate && python .\\translator.py`;
-    exec(pythonCommand, (pythonError, pythonStdout, pythonStderr) => {
-      if (pythonError) {
-        console.error(`Error executing Python script: ${pythonError}`);
-        return;
-      }
-         
-      console.log(`Python script output: ${pythonStdout}`);
-    });
-  });
+  try {
+    // PowerShell 스크립트 실행 (conda 가상환경 실행 및 oemer 실행)
+    const { stdout: psResult } = await execAsync(`powershell -Command ".\\script.ps1"`);
+    console.log(`파워쉘(oemer) 실행결과:\n${psResult}`);
+    
+    // Python 스크립트 실행 (musicxml -> wav 변환 스크립트 실행)
+    const { stdout: pyResult } = await execAsync(`.\\newenv\\Scripts\\activate && python .\\translator.py`)
+    console.log(`파이썬(translator) 실행결과: ${pyResult}`);
+
+    return musicTitle.split(".")[0];
+
+  } catch (error) {
+    console.log("에러가 발생했습니다! : ", error);
+    return "Error";
+  }
 }
 
 export default Core;
-
-Core();
